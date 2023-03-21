b9c4955f4344830c49334d6bb6ad0d1eda3ec532
Andrey <maar32@yandex.ru>
2023-03-21 12:44:02 +0300

    v. 1.0

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,22 +1,31 @@
 package web.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import web.dao.CarDao;
-import web.dao.CarDaoImpl;
 import web.model.Car;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 @Service
 public class CarServiceImpl implements CarService{
 
-    private final CarDao carDao = new CarDaoImpl();
+    private final List<Car> cars = new ArrayList<>(Arrays.asList(
+            new Car("Car1", 1, 2011),
+            new Car("Car2", 2, 2012),
+            new Car("Car3", 3, 2013),
+            new Car("Car4", 4, 2014),
+            new Car("Car5", 5, 2015)
+    ));
 
     @Override
     public List<Car> getCars(int count) {
-        return carDao.getCars(count);
+        if (count > cars.size()) {
+            return cars;
+        } else if (count < 0) {
+            return new ArrayList<>();
+        }
+        return cars.subList(0, count);
     }
 }
